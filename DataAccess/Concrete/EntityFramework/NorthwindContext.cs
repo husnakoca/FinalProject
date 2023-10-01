2f7f7619376bee2026f3ecba7624ce8a2d88f4fa
husnakoca <husnakoca0@gmail.com>
2023-10-01 03:03:29 +0300

    1 oct 2

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,13 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=175.45.2.12");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)mssqllocaldb;Database=Northwind;Trusted_Connection=true");
         }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
     }
 }
+ 
